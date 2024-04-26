8229ed88bf9ca7b2d484d3e64510789c1bf97370
Max Gordon <max@gforge.se>
2024-04-26 08:30:31 +0200

    Refactor ChartTabs component and add MedicationsTable and LabValueTable

--- src/components/ChartTabs/index.tsx
+++ src/components/ChartTabs/index.tsx
@@ -1,5 +1,8 @@
-import {Tab, Tabs} from '@mui/material';
+import { Tab, Tabs } from '@mui/material';
 import { useState } from 'react';
+
+import { LabValueTable } from '../LabValues';
+import { MedicationsTable } from '../Medications';
 
 type ChartTabsProps = {
     medications: string[];
@@ -7,24 +10,53 @@ type ChartTabsProps = {
     notes: string[];
 };
 
+const TabPanel = ({
+    children,
+    value,
+    id,
+}: {
+    value: string;
+    id: string;
+    children: React.ReactNode;
+}) => {
+    return (
+        <div role="tabpanel" id={`tabpanel-${id}`} hidden={value !== id}>
+            {children}
+        </div>
+    );
+};
+
 export const ChartTabs = ({
     medications,
     labvalues,
     notes,
 }: ChartTabsProps) => {
-    const [activeTab, setActiveTab] = useState("Notes");
+    const [activeTab, setActiveTab] = useState('Notes');
 
     return (
-        <Tabs
-            value={activeTab}
-            onChange={(_, newValue) => setActiveTab(newValue)}
-            variant="scrollable"
-            scrollButtons="auto"
-            aria-label="scrollable auto tabs example"
-        >
-            <Tab label="Notes" />
-            <Tab label="Medications" />
-            <Tab label="Lab Values" />
-        </Tabs>
+        <>
+            <Tabs
+                value={activeTab}
+                onChange={(_, newValue) => setActiveTab(newValue)}
+                variant="scrollable"
+                scrollButtons="auto"
+                aria-label="scrollable auto tabs example"
+            >
+                <Tab label="Notes" />
+                <Tab label="Medications" />
+                <Tab label="Lab Values" />
+            </Tabs>
+            <TabPanel value={activeTab} id="Notes">
+                {notes.map((note, i) => (
+                    <div key={i}>{note}</div>
+                ))}
+            </TabPanel>
+            <TabPanel value={activeTab} id="Medications">
+                <MedicationsTable medications={medications} />
+            </TabPanel>
+            <TabPanel value={activeTab} id="Lab Values">
+                <LabValueTable labvalues={labvalues} />
+            </TabPanel>
+        </>
     );
 };
