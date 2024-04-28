fd0482a4519c6d7c1ce74a7cb23a3ea6c6b7f496
Max Gordon <max@gforge.se>
2024-04-28 23:41:12 +0200

    Refactor loadData function to loadCharts2Translate in loadData.ts

--- src/helpers/loadData.ts
+++ src/helpers/loadData.ts
@@ -1,21 +1,16 @@
-import { fullChartSchema } from 'validators';
-import { InferType } from 'yup';
-
-// Define the expected structure of your JSON files if known
-type FullChartData = {
-    case_id: string;
-    specialty: string;
-    chart: InferType<typeof fullChartSchema>;
-}
+import { FullChart2Summarise, fullChartSchema } from 'validators';
 
 // Update the resolver type to reflect that it resolves to a module with a `default` property
-type LoadedData = Record<string, FullChartData>;
+type LoadedData = Record<string, FullChart2Summarise>;
 
-export const loadData = (): LoadedData => {
-    const modulePaths = import.meta.glob<{ default: unknown}>('../data/*.json', { eager: true });
+export const loadCharts2Translate = (): LoadedData => {
+    const modulePaths = import.meta.glob<{ default: unknown }>(
+        '../data/*.json',
+        { eager: true }
+    );
 
     const modules = Object.entries(modulePaths)
-        .map(([path, data]): [string, FullChartData] | null => {
+        .map(([path, data]): [string, FullChart2Summarise] | null => {
             if (typeof path !== 'string') return null;
 
             const filename = path.split('/').pop();
@@ -24,17 +19,22 @@ export const loadData = (): LoadedData => {
             const moduleName = filename.replace('.json', '');
             try {
                 const chart = fullChartSchema.validateSync(data.default);
-                return [moduleName, {
-                    case_id: moduleName.replace(/.+(Case \d+).+/, "$1"),
-                    specialty: moduleName.replace(/raw_([^_]+).+/, "$1"),
-                    chart,
-                }];                
+                return [
+                    moduleName,
+                    {
+                        case_id: moduleName.replace(/.+(Case \d+).+/, '$1'),
+                        specialty: moduleName.replace(/raw_([^_]+).+/, '$1'),
+                        chart,
+                    },
+                ];
             } catch (error) {
                 console.warn(`Error loading ${moduleName}: ${error}`);
                 return null;
             }
         })
-        .filter((module): module is [string, FullChartData] => module !== null);
+        .filter(
+            (module): module is [string, FullChart2Summarise] => module !== null
+        );
 
     return Object.fromEntries(modules);
 };
