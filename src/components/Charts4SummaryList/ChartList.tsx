234eac4e27de4dcea019523b0e9f0a6902920f12
Max Gordon <max@gforge.se>
2024-05-05 22:16:13 +0200

    Added ability to select specialties

--- src/components/Charts4SummaryList/ChartList.tsx
+++ src/components/Charts4SummaryList/ChartList.tsx
@@ -1,6 +1,8 @@
 import {
+    Box,
     Chip,
     Paper,
+    Stack,
     styled,
     Table,
     TableBody,
@@ -10,6 +12,7 @@ import {
     Typography,
 } from '@mui/material';
 import { getChartId } from 'helpers';
+import { useMemo, useState } from 'react';
 
 import { ChartListRow } from './ChartListRow';
 import { SummaryChartListProps } from './types';
@@ -38,72 +41,116 @@ export const Chart4SummaryList = ({
     charts,
     summarise,
 }: SummaryChartListProps) => {
-    if (!charts.length) return null;
+    const { specialtyKeys, specialtiesByLanguage } = useMemo(() => {
+        let specialtiesByLanguage = charts.reduce(
+            (acc, chart) => {
+                const { specialty, language } = chart;
+                const key = `${specialty}@${language}`;
+                if (!acc[key]) {
+                    acc[key] = [];
+                }
+                acc[key].push(chart);
+                return acc;
+            },
+            {} as Record<string, SummaryChartListProps['charts']>
+        );
+        specialtiesByLanguage = Object.fromEntries(
+            Object.entries(specialtiesByLanguage).sort(([a], [b]) => {
+                // "original" is always first
+                if (a === 'original') return -1;
+                if (b === 'original') return 1;
+                return a.localeCompare(b);
+            })
+        );
+        return {
+            specialtyKeys: Array.from(
+                new Set(charts.map((chart) => chart.specialty))
+            ),
+            specialtiesByLanguage,
+        };
+    }, [charts]);
+    const [selectedSpecialties, setSelectedSpecialties] =
+        useState(specialtyKeys);
 
-    // Group by specialty
-    let specialtiesByLanguage = charts.reduce(
-        (acc, chart) => {
-            const { specialty, language } = chart;
-            const key = `${specialty}@${language}`;
-            if (!acc[key]) {
-                acc[key] = [];
-            }
-            acc[key].push(chart);
-            return acc;
-        },
-        {} as Record<string, SummaryChartListProps['charts']>
-    );
-    specialtiesByLanguage = Object.fromEntries(
-        Object.entries(specialtiesByLanguage).sort(([a], [b]) => {
-            // "original" is always first
-            if (a === 'original') return -1;
-            if (b === 'original') return 1;
-            return a.localeCompare(b);
-        })
-    );
+    if (!charts.length) return null;
 
     return (
         <Paper sx={{ padding: '10px' }}>
-            <Typography variant="h6">Charts for Summary</Typography>
-            <Typography variant="body1">Select a chart to summarise</Typography>
-            {Object.entries(specialtiesByLanguage).map(([key, charts]) => (
-                <TableContainer key={key}>
-                    <Typography variant="h6">
-                        {key.replace(/@.+/, '')}
+            <Box sx={{ marginBottom: '10px' }}>
+                <Typography variant="h6">Charts for Summary</Typography>
+                <Typography variant="body1">
+                    Select a chart to summarise
+                </Typography>
+                <Stack direction="row" gap={1}>
+                    {Array.from(specialtyKeys).map((specialty) => (
                         <Chip
-                            label={
-                                key.replace(/^.+@/, '') === 'original'
-                                    ? 'Original'
-                                    : key.replace(/^.+@/, '')
+                            key={specialty}
+                            label={specialty}
+                            onClick={() => {
+                                if (selectedSpecialties.includes(specialty)) {
+                                    setSelectedSpecialties(
+                                        selectedSpecialties.filter(
+                                            (s) => s !== specialty
+                                        )
+                                    );
+                                } else {
+                                    setSelectedSpecialties([
+                                        ...selectedSpecialties,
+                                        specialty,
+                                    ]);
+                                }
+                            }}
+                            color={
+                                selectedSpecialties.includes(specialty)
+                                    ? 'primary'
+                                    : 'default'
                             }
-                            sx={{ marginLeft: '1rem' }}
                         />
-                    </Typography>
-                    <Table
-                        style={{
-                            marginBottom: '1em',
-                        }}
-                    >
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Case ID</TableCell>
-                                <TableCell>Language</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {charts.map((chart) => (
-                                <ChartListRow
-                                    key={getChartId(chart)}
-                                    chart={chart}
-                                    summarise={summarise}
-                                />
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            ))}
+                    ))}
+                </Stack>
+            </Box>
+            {Object.entries(specialtiesByLanguage)
+                .filter(([specialty]) =>
+                    selectedSpecialties.includes(specialty.replace(/@.+/, ''))
+                )
+                .map(([key, charts]) => (
+                    <TableContainer key={key}>
+                        <Typography variant="h6">
+                            {key.replace(/@.+/, '')}
+                            <Chip
+                                label={
+                                    key.replace(/^.+@/, '') === 'original'
+                                        ? 'Original'
+                                        : key.replace(/^.+@/, '')
+                                }
+                                sx={{ marginLeft: '1rem' }}
+                            />
+                        </Typography>
+                        <Table
+                            style={{
+                                marginBottom: '1em',
+                            }}
+                        >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Case ID</TableCell>
+                                    <TableCell>Language</TableCell>
+                                    <TableCell>Actions</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {charts.map((chart) => (
+                                    <ChartListRow
+                                        key={getChartId(chart)}
+                                        chart={chart}
+                                        summarise={summarise}
+                                    />
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                ))}
         </Paper>
     );
 };
