d0fa8ef073367ab4120c4c019efb4b6300d638a8
Max Gordon <max@gforge.se>
2024-05-05 22:16:36 +0200

    Improved scroll behavior in ShowDetails component

--- src/components/ChartTabs/ShowDetails/index.tsx
+++ src/components/ChartTabs/ShowDetails/index.tsx
@@ -1,5 +1,6 @@
 import { Collapse, Stack } from '@mui/material';
 import dayjs from 'dayjs';
+import { useEffect, useRef } from 'react';
 import { LabValue, MedicationValue } from 'validators';
 
 import { LabTable } from './LabTable';
@@ -21,35 +22,60 @@ export const ShowDetails = ({
     labValues,
     currentDay,
     first,
-}: ShowDetailsProp) => (
-    <Stack
-        direction="row"
-        gap={2}
-        alignItems="start"
-        useFlexGap
-        flexWrap="wrap"
-    >
-        <Collapse
-            in={showLab}
-            unmountOnExit
-            orientation={showMedication ? 'horizontal' : 'vertical'}
-        >
-            <LabTable
-                labValues={labValues}
-                currentDay={currentDay}
-                first={first}
-            />
-        </Collapse>
+}: ShowDetailsProp) => {
+    const labRef = useRef<HTMLDivElement>(null);
+    const medicationRef = useRef<HTMLDivElement>(null);
 
-        <Collapse
-            in={showMedication}
-            unmountOnExit
-            orientation={showLab ? 'horizontal' : 'vertical'}
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (showLab && labRef.current) {
+                labRef.current.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'nearest',
+                });
+            }
+            if (showMedication && medicationRef.current) {
+                medicationRef.current.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'nearest',
+                });
+            }
+        }, 500);
+        return () => clearTimeout(timer); // Clean up the timer
+    }, [showLab, showMedication]);
+
+    return (
+        <Stack
+            direction="row"
+            gap={2}
+            alignItems="start"
+            useFlexGap
+            flexWrap="wrap"
         >
-            <MedicationTable
-                medications={medications}
-                currentDay={currentDay}
-            />
-        </Collapse>
-    </Stack>
-);
+            <Collapse
+                in={showLab}
+                unmountOnExit
+                orientation={showMedication ? 'horizontal' : 'vertical'}
+            >
+                <LabTable
+                    labValues={labValues}
+                    currentDay={currentDay}
+                    first={first}
+                    ref={labRef}
+                />
+            </Collapse>
+
+            <Collapse
+                in={showMedication}
+                unmountOnExit
+                orientation={showLab ? 'horizontal' : 'vertical'}
+                ref={medicationRef}
+            >
+                <MedicationTable
+                    medications={medications}
+                    currentDay={currentDay}
+                />
+            </Collapse>
+        </Stack>
+    );
+};
