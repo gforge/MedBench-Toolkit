4bc0ef3f7bee4ee1facd73b6eaabb95f80ae0118
Max Gordon <max@gforge.se>
2024-05-28 21:52:50 +0200

    Eslint and package updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
         'plugin:storybook/recommended',
+        'prettier',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
@@ -18,4 +19,4 @@ module.exports = {
             { allowConstantExport: true },
         ],
     },
-}
+};
